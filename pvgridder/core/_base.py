b50e095654a0ee2391ef3052089db582af3deece
Keurfon Luu <keurfonluu@outlook.com>
2025-07-20 23:17:28 +0700

    replace subitem cell array with more general layerid

--- pvgridder/core/_base.py
+++ pvgridder/core/_base.py
@@ -434,6 +434,7 @@ class MeshStackBase(MeshBase):
 
         # Generate submeshes
         meshes = []
+        n_layers = 0
 
         for i, (item1, item2) in enumerate(zip(self.items[:-1], self.items[1:])):
             mesh_a = item1.mesh.copy()
@@ -455,9 +456,12 @@ class MeshStackBase(MeshBase):
                     np.arange(mesh_a.n_cells), nsub
                 ).copy()
 
+            mesh_b.cell_data["LayerId"] = np.repeat(
+                np.arange(nsub) + n_layers, repeats
+            ).copy()
             mesh_b.cell_data["StackItem"] = np.full(mesh_b.n_cells, i)
-            mesh_b.cell_data["StackSubItem"] = np.repeat(np.arange(nsub), repeats)
             meshes.append(mesh_b)
+            n_layers += nsub
 
         # Merge submeshes
         mesh = merge(meshes, axis=self.axis, merge_points=False)
