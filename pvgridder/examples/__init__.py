ba5d2e871d033d58510e19d170a008db0944f7cd
Keurfon Luu <keurfonluu@outlook.com>
2025-08-19 13:58:49 +0700

    add convex and concave polyhedron cell examples

--- pvgridder/examples/__init__.py
+++ pvgridder/examples/__init__.py
@@ -3,6 +3,8 @@
 from .examples import (
     load_anticline_2d,
     load_anticline_3d,
+    load_concave_polyhedron,
+    load_half_stadium,
     load_topographic_terrain,
     load_well_2d,
     load_well_3d,
